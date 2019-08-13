7bcc2bf768f568db75fdd29d48249fd5c539fcaa
fakhriabdullah <fakhri.abdullah9496@gmail.com>
2019-08-13 22:41:03 +0700

    xml login

---
+++ app/src/main/java/com/hudipo/pum_indomaret/features/login/view/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.hudipo.pum_indomaret.features.login.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hudipo.pum_indomaret.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
