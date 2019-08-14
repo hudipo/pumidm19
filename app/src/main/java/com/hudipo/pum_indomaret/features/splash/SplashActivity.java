a6c279ad2f214e6499dda6f8149c7f559aac290c
rhmn68 <auliar68@gmail.com>
2019-08-14 23:04:27 +0700

    add view/splash

---
+++ app/src/main/java/com/hudipo/pum_indomaret/features/splash/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.hudipo.pum_indomaret.features.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.hudipo.pum_indomaret.R;
+import com.hudipo.pum_indomaret.features.login.view.LoginActivity;
+import com.hudipo.pum_indomaret.utils.StartActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        delayAndGoTo();
+    }
+
+    private void delayAndGoTo() {
+        new Handler().postDelayed(() -> {
+            StartActivity.goTo(this, LoginActivity.class);
+            finish();
+        },1200);
+    }
+}
