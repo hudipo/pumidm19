b6f14b21cfba0fb3cd5bb699837e690cef40ddc0
adekurniawan <adekurniawan@student.upi.edu>
2019-08-18 21:27:47 +0700

    view/register ready to be reviewed

--- app/src/main/java/com/hudipo/pum_indomaret/features/register/view/RegisterActivity.java
+++ app/src/main/java/com/hudipo/pum_indomaret/features/register/view/RegisterActivity.java
@@ -4,17 +4,13 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.hudipo.pum_indomaret.R;
