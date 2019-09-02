7a5a69d191b9805c8ce6783e0300f9f6bee5af6a
rhmn68 <auliar68@gmail.com>
2019-09-02 15:21:58 +0700

    add home

---
+++ app/src/main/java/com/hudipo/pum_indomaret/features/home/HomeActivity.java
@@ -0,0 +1,52 @@
+package com.hudipo.pum_indomaret.features.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.hudipo.pum_indomaret.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnRequest)
+    public void clickRequest(View view){
+        Toast.makeText(this, "Request", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.btnApproval)
+    public void clickApproval(View view){
+        Toast.makeText(this, "Approval", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.btnInbox)
+    public void clickInbox(View view){
+        Toast.makeText(this, "Inbox", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.btnResponse)
+    public void clickResponse(View view){
+        Toast.makeText(this, "Response", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.btnSetting)
+    public void clickSetting(View view){
+        Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.btnReport)
+    public void clickReport(View view){
+        Toast.makeText(this, "Report", Toast.LENGTH_SHORT).show();
+    }
+}
