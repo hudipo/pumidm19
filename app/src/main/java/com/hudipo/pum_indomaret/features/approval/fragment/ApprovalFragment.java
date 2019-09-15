29cc289fab37fb2b0d89218c534c7328c4434a56
fakhriabdullah <fakhri.abdullah9496@gmail.com>
2019-09-15 13:51:49 +0700

    dialog

--- app/src/main/java/com/hudipo/pum_indomaret/features/approval/fragment/ApprovalFragment.java
+++ app/src/main/java/com/hudipo/pum_indomaret/features/approval/fragment/ApprovalFragment.java
@@ -1,17 +1,25 @@
 package com.hudipo.pum_indomaret.features.approval.fragment;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -24,19 +32,17 @@ import com.hudipo.pum_indomaret.model.approval.ApprovalModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ApprovalFragment extends Fragment implements ApprovalContract.ApprovalView {
     @BindView(R.id.rvApproval)
     RecyclerView rvApproval;
     @BindView(R.id.cbApproval)
     ImageView cbApproval;
-    @BindView(R.id.btnApprove)
-    LinearLayout btnApprove;
-    @BindView(R.id.btnReject)
-    LinearLayout btnReject;
     @BindView(R.id.llAction)
     LinearLayout llAction;
 
@@ -73,6 +79,56 @@ public class ApprovalFragment extends Fragment implements ApprovalContract.Appro
             approvalAdapter.setAllChecked(isCheckedAll);
             initAction();
         });
+    }
+
+    @OnClick(R.id.btnApprove)
+    void approve(){
+        final AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+        alert.setTitle(getString(R.string.approve));
+        alert.setMessage(getString(R.string.message_dialog_approve));
+        alert.setPositiveButton(getString(R.string.yes), ((dialogInterface, i) -> {
+            // TODO: 15/09/19 to pin activity
+            Toast.makeText(getActivity(), "To PIN Activity", Toast.LENGTH_LONG).show();
+        }));
+        alert.setNegativeButton(getString(R.string.no), (dialogInterface, i) -> dialogInterface.dismiss());
+        alert.show();
+    }
+
+
+    @OnClick(R.id.btnReject)
+    void reject(){
+        FrameLayout container = new FrameLayout(Objects.requireNonNull(getActivity()));
+
+        container.addView(generateEditText());
+
+        final AlertDialog.Builder alert = new AlertDialog.Builder(Objects.requireNonNull(getActivity()));
+        alert.setTitle(getString(R.string.reject));
+        alert.setMessage(getString(R.string.message_dialog_reject));
+        alert.setPositiveButton(getString(R.string.yes), ((dialogInterface, i) -> {
+            // TODO: 15/09/19 to pin activity
+            Toast.makeText(getActivity(), "To PIN Activity", Toast.LENGTH_LONG).show();
+        }));
+        alert.setNegativeButton(getString(R.string.no), (dialogInterface, i) -> dialogInterface.dismiss());
+        alert.setView(container);
+        alert.show();
+    }
+
+    private EditText generateEditText()
+    {
+        int padding = getResources().getDimensionPixelSize(R.dimen.min_margin);
+        int margin = getResources().getDimensionPixelSize(R.dimen.main_margin);
+        FrameLayout.LayoutParams params = new  FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.leftMargin = margin;
+        params.rightMargin = margin;
+
+        final EditText input = new EditText(getActivity());
+        input.setLayoutParams(params);
+        input.setBackground(Objects.requireNonNull(getActivity()).getDrawable(R.drawable.bg_rounded_border_black_solid_black));
+        input.setHint(getString(R.string.hint_dialog_reject));
+        input.setMinLines(3);
+        input.setGravity(Gravity.TOP);
+        input.setPadding(padding, padding, padding, padding);
+        return input;
     }
 
     @Override
