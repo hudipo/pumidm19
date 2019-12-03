77b86da1284967be610ab688d2c915cac420dd6c
rhmn68 <auliar68@gmail.com>
2019-12-03 15:45:01 +0700

    add search but still failed

--- app/src/main/java/com/hudipo/pum_indomaret/adapter/SearchDeptAdapter.java
+++ app/src/main/java/com/hudipo/pum_indomaret/adapter/SearchDeptAdapter.java
@@ -1,8 +1,11 @@
 package com.hudipo.pum_indomaret.adapter;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,15 +13,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.hudipo.pum_indomaret.R;
 import com.hudipo.pum_indomaret.model.departement.DepartmentItem;
-import com.hudipo.pum_indomaret.model.departement.DepartmentResponse;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SearchDeptAdapter extends RecyclerView.Adapter<SearchDeptAdapter.ViewHolder>{
+public class SearchDeptAdapter extends RecyclerView.Adapter<SearchDeptAdapter.ViewHolder> implements Filterable {
 
-    private DepartmentResponse departmentResponse;
+    private List<DepartmentItem> listDepartment;
+    private List<DepartmentItem> filterDepartment;
     private ItemClickListener itemClickListener;
+
+    public void setListDepartment(ArrayList<DepartmentItem> listDepartment) {
+        this.listDepartment = listDepartment;
+        this.filterDepartment = listDepartment;
+        notifyDataSetChanged();
+    }
 
     public interface ItemClickListener{
         void onClick(DepartmentItem departmentItem);
@@ -28,11 +40,6 @@ public class SearchDeptAdapter extends RecyclerView.Adapter<SearchDeptAdapter.Vi
         this.itemClickListener = itemClickListener;
     }
 
-    public void setDepartmentResponse(DepartmentResponse departmentResponse) {
-        this.departmentResponse = departmentResponse;
-        notifyDataSetChanged();
-    }
-
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -40,13 +47,51 @@ public class SearchDeptAdapter extends RecyclerView.Adapter<SearchDeptAdapter.Vi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.bindItem(departmentResponse.getDepartment().get(position));
+    public int getItemCount() {
+        if (filterDepartment == null){
+            return 0;
+        }
+        return filterDepartment.size();
     }
 
     @Override
-    public int getItemCount() {
-        return departmentResponse.getDepartment().size();
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bindItem(filterDepartment.get(position));
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                String charSearch = constraint.toString().toLowerCase().trim();
+                if (charSearch.isEmpty()){
+                    filterDepartment = listDepartment;
+                }else {
+                    List<DepartmentItem> filteredList = new ArrayList<>();
+                    if (listDepartment != null){
+                        for (DepartmentItem row : listDepartment){
+                            if (row.getDescription().toLowerCase().contains(charSearch)){
+                                filteredList.add(row);
+                            }
+                        }
+                        filterDepartment.clear();
+                        filterDepartment.addAll(filteredList);
+                        Log.d("coba", "size filter: "+filteredList.size());
+                    }
+                }
+                Log.d("coba", "size: "+filterDepartment.size());
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = filterDepartment;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                filterDepartment = (ArrayList<DepartmentItem>) results.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
