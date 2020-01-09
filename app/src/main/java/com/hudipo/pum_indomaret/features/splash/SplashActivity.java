4e7abf653a122c30ef9a9c20c00e68b8ec2da365
hudipo <dannyhp2016@gmail.com>
2020-01-09 20:51:58 +0700

    splash

--- app/src/main/java/com/hudipo/pum_indomaret/features/splash/SplashActivity.java
+++ app/src/main/java/com/hudipo/pum_indomaret/features/splash/SplashActivity.java
@@ -16,9 +16,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        delayAndGoTo();
         checkLogin();
-//        delayAndGoTo();
+
     }
 
     private void checkLogin() {
